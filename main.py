fbddad8ab25528532270c400919a426317e591aa
dushan123 <121484805+dushan123@users.noreply.github.com>
2023-01-06 23:18:23 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 class WorkPeice():
-    def __init__(self, material, shape, radius=None, length=None, width=None, height=None):
+    def __init__(self, material, shape, radius=None, length=None, width=None, height=None, base=None, height_P=None):
         
         self.material = material
         self.shape = shape
@@ -7,6 +7,8 @@ class WorkPeice():
         self.length  = length
         self.width = width
         self.height=height
+        self.base=base
+        self.height_P=height_P
         
 
     def __add__(self, other):
@@ -41,8 +43,10 @@ class WorkPeice():
 wooden_top = WorkPeice(material='wood',shape='Cylider', radius=1, height=0.1)
 metal_leg = WorkPeice(material='steel',shape='Cylider', radius=0.1, height=2)
 metal_cubic=WorkPeice(material='steel',shape='Cubic', length=1, width=3,height=2)
-chair=wooden_top.__add__(metal_leg)
-print (wooden_top.radius)
+prism_shape= WorkPeice(material='steel', shape= 'Prism', base=2, height_P=3,height=10)
+
+#chair=wooden_top.__add__(metal_leg)
+#print (wooden_top.radius)
 
 wooden_top_volume=wooden_top.print_material_volume_breakdown()
 metal_leg_volume=metal_leg.print_material_volume_breakdown()
