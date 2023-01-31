4c44ece2ca7118e9281ea7d26a990c7e2500275c
dushan123 <121484805+dushan123@users.noreply.github.com>
2023-01-31 15:40:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,71 +1,92 @@
-class WorkPeice():
-    def __init__(self, material, shape, radius=None, length=None, width=None, height=None, base=None, height_P=None):
-        
-        self.material = material
-        self.shape = shape
-        self.radius   = radius
-        self.length  = length
-        self.width = width
-        self.height=height
-        self.base=base
-        self.height_P=height_P
-        
+class CompositeObject:
+  
+  def __init__(self, object1=None, object2=None):
+    self.object1 = object1
+    self.object2 = object2
 
-    def __add__(self, other):
-      Volume= self.height + other.height
+  def __add__(self, other):
+    Composite = CompositeObject(self, other)
+    return Composite
+
+  def get_volume(self):
+      composite_volume = 0
+      objects = [self.object1.print_object_volume(),self.object2.print_object_volume()]
+
+      for x in objects:
+        
+        composite_volume = composite_volume + x
+        
+      return print(composite_volume)
+
+class WorkPeice:
+
+  def __init__(self,material,shape,radius=None,length=None,width=None,height=None,base=None,height_p=None):
+
+    self.material = material
+    self.shape = shape
+    self.radius = radius
+    self.length = length
+    self.width = width
+    self.height = height
+    self.base = base
+    self.height_p = height_p
+    
+  def __add__(self, other):
+    Composite = CompositeObject(self, other)
+    return Composite
+
+  def __mul__(self, other):
+    Composite2 = CompositeObject(self,other)
+    return Composite2
+
+  def print_object_volume(self):
+    if self.shape == 'Cylider':
+      Volume = 3.14 * self.radius**2 * self.height
       return Volume
-       
 
-    def print_material_volume_breakdown(self):
-      if self.shape == 'Cylider':
-        Volume= 3.14* self.radius**2*self.height
-        return  Volume 
-         
-      elif self.shape == 'Cubic':
-        Volume= self.length*self.width*self.height
-        return  Volume
-        
-      elif self.shape == 'Prism':
-        Volume= self.base*self.length*self.height_P*self.height*0.5
-        return  Volume
-      else:
-        print("Shape Error")
+    elif self.shape == 'Cubic':
+      Volume = self.length * self.width * self.height
+      return Volume
 
-    def Total_cost(self):
-      if self.material == 'wood':
-        cost= WorkPeice.print_material_volume_breakdown(self)*200 #UnitCost_of_Wood
-        return  cost
-      elif self.material == 'steel':
-        cost= WorkPeice.print_material_volume_breakdown(self)*2200 #UnitCost_of_steel
-        return  cost
+    elif self.shape == 'Prism':
+      Volume = self.base * self.height_p * self.height * 0.5
+      return Volume
+
+    else:
+      print("Shape Error")
+
+  def total_cost(self):
+    if self.material == 'wood':
+      cost = self.print_object_volume() * 200  #UnitCost of Wood
+      return cost
       
+    elif self.material == 'steel':
+      cost = self.print_object_volume() * 2200  #UnitCost of steel
+      return cost
 
-wooden_top = WorkPeice(material='wood',shape='Cylider', radius=1, height=0.1)
-metal_leg = WorkPeice(material='steel',shape='Cylider', radius=0.1, height=2)
-metal_cubic=WorkPeice(material='steel',shape='Cubic', length=1, width=3,height=2)
-prism_shape= WorkPeice(material='steel', shape= 'Prism', base=2, height_P=3,height=10)
 
-#chair=wooden_top.__add__(metal_leg)
-#print (wooden_top.radius)
+wooden_top = WorkPeice(material='wood', shape='Cylider', radius=1, height=0.1)
+metal_leg = WorkPeice(material='steel', shape='Cylider', radius=0.1, height=3)
+metal_cubic = WorkPeice(material='steel',shape='Cubic',length=1,width=3,height=2)
+prism_shape = WorkPeice(material='steel',shape='Prism',base=2,height_p=3,height=10)
 
-wooden_top_volume=wooden_top.print_material_volume_breakdown()
-metal_leg_volume=metal_leg.print_material_volume_breakdown()
+wooden_top_cost = wooden_top.total_cost()
+metal_leg_cost = metal_leg.total_cost()
 
-wooden_top_cost=wooden_top.Total_cost()
-metal_leg_cost=metal_leg.Total_cost()
+print(wooden_top.print_object_volume())
+print(metal_leg.print_object_volume())
+print(metal_cubic.print_object_volume())
+print(prism_shape.print_object_volume())
 
-print(metal_cubic.print_material_volume_breakdown())
-print(wooden_top_cost)
-print(metal_leg_cost)
-print(wooden_top_volume)
-print(metal_leg_volume)
-print(chair)
+composite1 = wooden_top + metal_leg
+chair= wooden_top + metal_leg*4
+composite2= composite1 + prism_shape
+composite3 = composite1 + composite2
 
-#print_material_volume_breakdown()
+print(composite1)
+print(composite2)
+print(composite3)
 
-#wooden_top = WorkPiece(material="Wood", shape="Cylinder", radius=1, height=0.1)
-#metal_leg = WorkPiece(material="Steel", shape="Cylinder", radius=0.1, height=2)
-#chair = wooden_top + 4 * metal_leg
+composite1.get_volume()
+#composite2.get_volume()    cant find this volume
 
-#chair.print_material_volume_breakdown()
-#chair.print_total_cost()
